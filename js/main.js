a02172e19dbb9b31863c611070e1590f09591a4e
youcan-jpn <66929068+youcan-jpn@users.noreply.github.com>
2020-09-02 15:27:07 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,88 @@
+$(function () {
+
+    /*
+     * Slideshow
+     */
+    // slideshow クラスを持った要素ごとに処理を実行
+    $('.slideshow').each(function () {
+
+        var $slides = $(this).find('img'), // すべてのスライド
+            slideCount = $slides.length,   // スライドの点数
+            currentIndex = 0;              // 現在のスライドを示すインデックス
+
+        // 1 番目のスライドをフェードインで表示
+        $slides.eq(currentIndex).fadeIn();
+
+        // 7500 ミリ秒ごとに showNextSlide 関数を実行
+        setInterval(showNextSlide, 7500);
+
+        // 次のスライドを表示する関数
+        function showNextSlide () {
+
+            // 次に表示するスライドのインデックス
+            // (もし最後のスライドなら最初に戻る)
+            var nextIndex = (currentIndex + 1) % slideCount;
+
+            // 現在のスライドをフェードアウト
+            $slides.eq(currentIndex).fadeOut();
+
+            // 次のスライドをフェードイン
+            $slides.eq(nextIndex).fadeIn();
+
+            // 現在のスライドのインデックスを更新
+            currentIndex = nextIndex;
+        }
+
+    });
+
+    /*
+     * Sticky header
+     */
+    $('.page-header').each(function () {
+
+        var $window = $(window), // ウィンドウを jQuery オブジェクト化
+            $header = $(this),   // ヘッダーを jQuery オブジェクト化
+            // ヘッダーのデフォルト位置を取得
+            headerOffsetTop = $header.offset().top;
+
+        // ウィンドウのスクロールイベントを監視
+        // (ウィンドウがスクロールするごとに処理を実行する)
+        $window.on('scroll', function () {
+            // ウィンドウのスクロール量をチェックし、
+            // ヘッダーのデフォルト位置を過ぎていればクラスを付与、
+            // そうでなければ削除
+            if ($window.scrollTop() > headerOffsetTop) {
+                $header.addClass('sticky');
+            } else {
+                $header.removeClass('sticky');
+            }
+        });
+
+        // ウィンドウのスクロールイベントを発生させる
+        // (ヘッダーの初期位置を調整するため)
+        $window.trigger('scroll');
+
+    });
+
+    /*
+    buttons
+    */
+   var duration = 300;
+
+   $('#buttons button').each(function(index){
+       var pos = index * 40 + 20;
+       $(this).css('top', pos);
+    })
+    .on('mouseover', function(){
+        var $btn = $(this).stop(true).animate({
+            backgroundColor: 'rgb(214, 33, 33)',
+            color: '#fff'
+        }, duration);
+    })
+    .on('mouseout', function(){
+        var $btn = $(this).stop(true).animate({
+            backgroundColor: '#fff',
+            color: '#000000'
+        }, duration);
+    })
+});
